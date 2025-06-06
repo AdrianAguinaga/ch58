582fe6ac2dcd7d96f5a9e7bd7c2ec4b1450117e7
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2025-06-05 18:22:08 -0700

    106-58

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+function start() {
+    console.log("App started");
+    stop();
+}
+
+function stop() {
+    console.log("App stopped");
+}
+
+function init() {
+    console.log("App initialized");
+    start();
+}
+
+window.onload = init;// this waits for the DOM to be fully loaded before running init 
+//it waits until the html and css are fully loaded before running the init function
